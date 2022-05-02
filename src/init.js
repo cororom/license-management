74f2c42215a7f5782dcbf033637d9778b0176a5a
root <root@c126.nskorea.com>
2022-05-02 22:20:51 +0900

    Updated Package Scripts

--- src/init.js
+++ src/init.js
@@ -1,9 +1,11 @@
+import "regenerator-runtime";
 import "dotenv/config";
 import "./db.js";
 import app from "./server";
 
 const PORT = process.env.PORT;
+const IP = process.env.IP;
 
-const handleListening = () => console.log(`🚀localhost:${PORT}🚀`);
+const handleListening = () => console.log(`🚀${IP}:${PORT}🚀`);
 
-app.listen(PORT, handleListening);
+app.listen(PORT, IP, handleListening);
